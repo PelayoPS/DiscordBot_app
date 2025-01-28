50608deba567e29088ec39f823bea6e0734822ed
Pelayo Palacio Suárez <63815361+PelayoPS@users.noreply.github.com>
2025-01-28 13:12:05 +0100

    update structure

--- src/main/java/bot/commands/modules/mod/Ban.java
+++ src/main/java/bot/commands/modules/mod/Ban.java
@@ -1,18 +1,21 @@
 package bot.commands.modules.mod;
 
+import bot.commands.ICommand;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
-public class Ban {
+public class Ban implements ICommand {
+
+    private String name = "ban";
 
     /**
      * Banea a un usuario.
      * 
      * @param event El evento de interacción del comando.
      */
-    public static void banUser(SlashCommandInteractionEvent event) {
+    public void execute(SlashCommandInteractionEvent event) {
         // TODO Implementar el baneo de un usuario
     }
 
@@ -21,11 +24,15 @@ public class Ban {
      * 
      * @return SlashCommandData La información del comando de slash.
      */
-    public static SlashCommandData getSlash() {
+    public SlashCommandData getSlash() {
         SlashCommandData slash = Commands.slash("ban", "Banear a un usuario")
                 .addOption(OptionType.USER, "usuario", "El usuario a banear", true)
                 .addOption(OptionType.STRING, "razon", "La razón del baneo", false);
         return slash;
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
