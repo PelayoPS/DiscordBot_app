827946e79dee4e7bd12ec90d2c5baaf405f55bf8
Pelayo Palacio Suárez <63815361+PelayoPS@users.noreply.github.com>
2025-01-28 13:16:17 +0100

    update structure

--- src/main/java/bot/commands/modules/ManageCommands.java
+++ src/main/java/bot/commands/modules/ManageCommands.java
@@ -1,18 +1,13 @@
 package bot.commands.modules;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import bot.events.EventListener;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 /**
  * Clase que maneja los comandos de gestión del bot.
  * Extiende de {@link EventListener} para manejar eventos de interacción de
  * comandos.
  */
-public class ManageCommands extends ICommandManager {
+public class ManageCommands extends CommandManager {
 
     /**
      * Constructor de la clase ManageCommands.
--- src/main/java/bot/commands/modules/UserCommands.java
+++ src/main/java/bot/commands/modules/UserCommands.java
@@ -1,18 +1,14 @@
 package bot.commands.modules;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import bot.events.EventListener;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
 /**
  * Clase que maneja los comandos de usuario del bot.
  * Extiende de {@link EventListener} para manejar eventos de interacción de
  * comandos.
  */
-public class UserCommands extends ICommandManager {
+public class UserCommands extends CommandManager {
 
     /**
      * Constructor de la clase UserCommands.
