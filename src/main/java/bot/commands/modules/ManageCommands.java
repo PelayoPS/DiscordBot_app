d408691100b7cc2a8a9375a634cc4bc8a761e87a
Pelayo Palacio Suárez <63815361+PelayoPS@users.noreply.github.com>
2025-02-05 14:26:54 +0100

    new commands

--- src/main/java/bot/commands/modules/ManageCommands.java
+++ src/main/java/bot/commands/modules/ManageCommands.java
@@ -1,5 +1,6 @@
 package bot.commands.modules;
 
+import bot.commands.modules.management.CreateRole;
 import bot.events.EventListener;
 
 /**
@@ -15,6 +16,6 @@ public class ManageCommands extends CommandManager {
      * Agrega los comandos de gestión a la lista de comandos.
      */
     public ManageCommands() {
-        // TODO Agregar los comandos de gestión a la lista de comandos
+        commands.add(new CreateRole());
     }
 }
